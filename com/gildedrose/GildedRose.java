881f19af863b1e866834862f600eddc7b4e0f783
Florian MORGAN <florian.morgan@gmail.com>
2014-07-23 09:17:31 +0200

    Some refacto

--- com/gildedrose/GildedRose.java
+++ com/gildedrose/GildedRose.java
@@ -13,57 +13,49 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-        	decreaseSellInItem(i);
-        	
-        	if (AGED_BRIE.equals(items[i].name)) {
-        		increaseQualityItem(i);
-        	} else {
-        		if (BACKSTAGE_PASSES.equals(items[i].name)) 
-        		{
-        			increaseQualityItem(i);
-        			
-        			if (items[i].quality < 50) {
-        				if (items[i].sellIn < 11) {
-        					increaseQualityItem(i);
-        				}
-        				
-        				if (items[i].sellIn < 6) {
-        					increaseQualityItem(i);
-        				}
-        			}
-        		} else {
-        			decreaseQualityItem(i);
-        		}
-            }
+            if (SULFURAS_HAND_OF_RAGNAROS.equals(items[i].name)) {
 
+            } else {
+                items[i].sellIn = items[i].sellIn - 1;
 
-            if (items[i].sellIn < 0) {
+                if (BACKSTAGE_PASSES.equals(items[i].name)) {
+                    increaseQualityItem(i);
+
+                    if (items[i].sellIn < 11) {
+                        increaseQualityItem(i);
+                    }
+
+                    if (items[i].sellIn < 6) {
+                        increaseQualityItem(i);
+                    }
+                }
+
                 if (AGED_BRIE.equals(items[i].name)) {
-                	increaseQualityItem(i);
-                } else {
-                	if (BACKSTAGE_PASSES.equals(items[i].name)) {
-                		items[i].quality = 0;
-                	} else {
-                		decreaseQualityItem(i);
-                	}
+                    increaseQualityItem(i);
+                } else if (!BACKSTAGE_PASSES.equals(items[i].name)) {
+                    decreaseQualityItem(i);
+                }
+
+                if (items[i].sellIn < 0) {
+                    if (BACKSTAGE_PASSES.equals(items[i].name)) {
+                        items[i].quality = 0;
+                    }
+
+                    if (AGED_BRIE.equals(items[i].name)) {
+                        increaseQualityItem(i);
+                    } else if (!BACKSTAGE_PASSES.equals(items[i].name)) {
+                        decreaseQualityItem(i);
+                    }
                 }
             }
         }
     }
 
     private void decreaseQualityItem(int i) {
-		if (items[i].quality > 0) {
-		    if (!SULFURAS_HAND_OF_RAGNAROS.equals(items[i].name)) {
-		        items[i].quality = items[i].quality - 1;
-		    }
-		}
-	}
-
-	private void decreaseSellInItem(int i) {
-		if (!SULFURAS_HAND_OF_RAGNAROS.equals(items[i].name)) {
-		    items[i].sellIn = items[i].sellIn - 1;
-		}
-	}
+        if (items[i].quality > 0) {
+            items[i].quality = items[i].quality - 1;
+        }
+    }
 
 	private void increaseQualityItem(int i) {
 		if (items[i].quality < 50) {
